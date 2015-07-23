e44eec60373ba068c06f257210901719bc454624
ckhawks <ckhawks@gmail.com>
2015-07-22 23:12:31 -0500

    it works now
    >>>

--- src/postData.js
+++ src/postData.js
@@ -7,6 +7,7 @@ Pebble.addEventListener("ready",
 Pebble.addEventListener('appmessage',
   function(e) {
     console.log('Received message: ' + JSON.stringify(e.payload));
+    console.log(e.payload.mtype);
     switch(e.payload.mtype){
       case 200:
         var req = new XMLHttpRequest();
@@ -27,8 +28,12 @@ Pebble.addEventListener('appmessage',
         break;
       case 1:
         console.log("I don't know what to do with case 1?"); // haha what
-        var req = new XMLHttpRequest();
-        req.open('GET', 'https://slack.com/api/chat.postMessage?token=xoxp-7213939015-8005854450-8065247734-2d7ab5&channel=%23random&text=BOT%3A%20Testing&username=ckhbot&pretty=1', true);
+        var req1 = new XMLHttpRequest();
+        req1.open('GET', 'https://slack.com/api/chat.postMessage?token=xoxp-7213939015-8005854450-8065247734-2d7ab5&channel=%23random&text=BOT%3A%20Testing&username=ckhbot&pretty=1', true);
+        req1.onload = function(e){
+          // do stuff.exe
+        };
+        req1.send(null);
         // doStuff(); .avi.gif.jpeg.zip.exe -- ACTUALLY NOT A VIRUS!1!.pepe.c.js.html.bmp ... "what's a bitmap?! guYS MC RECIPES HALP MC IS GETITNG DDOS SNED HLEP!"
         break;
     }
