cb3c5a39e4ec2415acdea0d287f08b84eb5ac688
ckhawks <ckhawks@gmail.com>
2015-07-22 22:20:38 -0500

    First Try
    Compiles but nothing happens.

---
+++ src/postData.js
@@ -0,0 +1,34 @@
+Pebble.addEventListener("ready",
+  function(e){
+    console.log("Javascript app ready and running!");
+  }
+);
+
+Pebble.addEventListener('appmessage',
+  function(e) {
+    console.log('Received message: ' + JSON.stringify(e.payload));
+    switch(e.payload.mtype){
+      case 0:
+        var req = new XMLHttpRequest();
+        req.open('GET', 'https://slack.com/api/chat.postMessage?token=xoxp-7213939015-8005854450-8065247734-2d7ab5&channel=%23random&text=BOT%3A%20Testing&username=ckhbot&pretty=1', true);
+        req.onload = function(e) {
+          if (req.readyState == 4 && req.status == 200) {
+            if(req.status == 200) {
+              var response = JSON.parse(req.responseText);
+              var temperature = response.list[0].main.temp;
+              var icon = response.list[0].main.icon;
+              Pebble.sendAppMessage({ 'icon':icon, 'temperature':temperature + '\u00B0C'});
+            } else { 
+              console.log('Error'); 
+            }
+          }
+        };
+        req.send(null);
+        break;
+      case 1:
+        console.log("I don't know what to do with case 1?"); // haha what
+        // doStuff(); .avi.gif.jpeg.zip.exe -- ACTUALLY NOT A VIRUS!1!.pepe.c.js.html.bmp ... "what's a bitmap?! guYS MC RECIPES HALP MC IS GETITNG DDOS SNED HLEP!"
+        break;
+    }
+  }
+);
